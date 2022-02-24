82e5d9a209c142f3ea21b50ae7f452823f36cdee
acessor <98286101+acessors@users.noreply.github.com>
2022-02-24 16:36:57 +0300

    add WMI Displayer visual studio project

---
+++ WMI Displayer/MainForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: админ
+ * Date: 23.02.2022
+ * Time: 15:38
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace WMI_Displayer
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 8);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(262, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(280, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "get props";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 39);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(365, 295);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 346);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "WMI Displayer by Acessor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+		}
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+	}
+}
---
+++ WMI Displayer/MainForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Management;
+
+namespace WMI_Displayer
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+
+        private void button1_Click(object sender, EventArgs e) {
+            richTextBox1.Text = getWmiData(comboBox1.Text);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e) {
+            foreach (string name in GetWmiNamespaces("root")) {
+                comboBox1.Items.Add(name);
+            }
+        }
+        
+        
+        private static string getWmiData(string cls) {
+            ManagementObjectSearcher mgmtObjSearcher = new ManagementObjectSearcher("SELECT * FROM "+cls);
+		    ManagementObjectCollection colDisks = mgmtObjSearcher.Get();
+            string info = "";
+            try {
+                foreach (ManagementObject objDisk in colDisks) {
+                    foreach (PropertyData data in objDisk.Properties) {
+                        info += data.Name + ": " + data.Value + "\n";
+                    }
+                }
+                return info;
+            }
+            catch (Exception e) {
+                return e.ToString();
+            }
+            
+        }
+
+        private static string[] GetWmiNamespaces(string root) {
+            List<String> namespaces = new List<string>();
+            ManagementObjectSearcher mgmtObjSearcher = new ManagementObjectSearcher("SELECT * FROM meta_class");
+            ManagementObjectCollection colDisks = mgmtObjSearcher.Get();
+            try {
+                foreach (ManagementObject objDisk in colDisks) {
+                    namespaces.Add(objDisk.ClassPath.ClassName);
+                }
+            }
+            catch (Exception e) {
+                return null;
+            }
+
+            return namespaces.ToArray();
+        }
+	}
+}
